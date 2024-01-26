f3514ce5820066d1870a2b3791e9360a7a2573a5
lohcy <loh.cheeyung96@gmail.com>
2024-01-26 13:32:44 +0800

    create AppBar in ChatPage

--- lib/pages/chat/controller.dart
+++ lib/pages/chat/controller.dart
@@ -2,7 +2,17 @@ import 'package:get/get.dart';
 import 'index.dart';
 
 class ChatController extends GetxController {
-  final state = ChatState();
+  ChatState state = ChatState();
   ChatController();
-  
+  var doc_id = null;
+
+  @override
+  void onInit() {
+    super.onInit();
+    var data = Get.parameters;
+    doc_id = data['doc_id'];
+    state.to_uid.value = data['to_uid'] ?? "";
+    state.to_name.value = data['to_name'] ?? "";
+    state.to_avatar.value = data['to_avatar'] ?? "";
+  }
 }
