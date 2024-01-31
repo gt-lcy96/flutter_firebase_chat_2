d8af3e4a115b2f879e3112b14b6111bba2783f69
lohcy <loh.cheeyung96@gmail.com>
2024-01-31 13:42:33 +0800

    add getFcmToken function

--- lib/pages/message/controller.dart
+++ lib/pages/message/controller.dart
@@ -4,6 +4,7 @@ import 'index.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:pull_to_refresh/pull_to_refresh.dart';
 import 'package:firebase_chat/common/entities/msg.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 
 class MessageController extends GetxController {
   MessageController();
@@ -15,6 +16,12 @@ class MessageController extends GetxController {
 
   final RefreshController refreshController =
       RefreshController(initialRefresh: true);
+
+  @override
+  void onReady() {
+    super.onReady();
+    getFcmToken();
+  }
 
   //required by RefreshController
   void onRefresh() {
@@ -57,6 +64,17 @@ class MessageController extends GetxController {
 
     if(to_messages.docs.isNotEmpty) {
       state.msgList.assignAll(to_messages.docs);
+    }
+  }
+
+  getFcmToken() async {
+    final fcmToken = await FirebaseMessaging.instance.getToken();
+    if(fcmToken != null) {
+      var user = await db.collection("users").where("id", isEqualTo: token).get();
+      if(user.docs.isNotEmpty) {
+        var doc_id = user.docs.first.id;
+        await db.collection("users").doc(doc_id).update({"fcmtoken": fcmToken});
+      }
     }
   }
 }
